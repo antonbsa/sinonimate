769d3be3381b85156420d137c461a49c80cac156
Anton <antonbsa@gmail.comAnton>
2020-07-10 15:44:27 -0300

    getting sentidos and sinonimos; charset adjusted

--- src/backend/app.js
+++ src/backend/app.js
@@ -7,22 +7,37 @@ const { send } = require('process')
 const hostname = 'https://www.sinonimos.com.br/'
 const path = 'teste'
 
-let body = ''
 
-request(`${hostname}${path}`, function(err, response, bd) {
-  body = bd
-  console.log(body)
-  console.log('Ok!')
+request({url: `${hostname}${path}`, encoding: 'binary'}, function(err, response, body) {
+
+if (err) return console.error(err)
+
+let $ = cheerio.load(body)
+
+console.log('Sentidos:')
+let sentido = $('div.sentido').each( function () {
+    console.log(`${$(this).text().replace(':', '')};`)
 })
 
-const $ = cheerio.load(body)
-console.log($)
+console.log()   //distanciar
+console.log('Sinonimos:')
+let sinonimos = $('a.sinonimo').each(function () {
+    console.log(`${$(this).text()};`)
+})
+
+// console.log(sinonimos.text())
+})
+
+
+
+
 const port = 3000
 
 app.get('/', function (req, res) {
-  
+
 })
 
 app.listen(port, function () {
-  console.log(`App rodando em http://localhost:${port}`)
+    console.log(`App rodando em http://localhost:${port}`)
+    console.log()   //distanciar
 })
